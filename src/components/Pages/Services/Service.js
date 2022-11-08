93b934258d657d76c45192fee827f2478c98fbb7
naim71 <naimkhan1314@gmail.com>
2022-11-09 03:56:11 +0600

    photo view installed

--- src/components/Pages/Services/Service.js
+++ src/components/Pages/Services/Service.js
@@ -1,34 +1,34 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {image, description, title, price} = service;
     return (
         <div>
-            <div className="overflow-hidden rounded bg-white text-slate-500 shadow-lg shadow-slate-200">
+            <div className="overflow-hidden rounded bg-white text-slate-500 shadow-lg shadow-slate-200" 
+            style={{height: "450px"}}>
                 {/*  <!-- Image --> */}
                 <figure>
                     <img
-                        src="https://i.ibb.co/dpRscpN/code-snapshot.png"
-                        alt="card image"
-                        className="aspect-video w-full"
-                    />
+                        src={image}
+                        alt=""
+                        className="aspect-video w-full" />
                 </figure>
                 {/*  <!-- Body--> */}
                 <div className="p-6">
                     <header className="mb-4">
                         <h3 className="text-xl font-medium text-slate-700">
-                            Greek breakfast
+                            {title}
                         </h3>
-                        <p className=" text-slate-400"> $8.99</p>
+                        <p className=" text-emerald-500"> $<span>{price}</span>.99</p>
                     </header>
-                    <p>
-                        Blueberry Superpower: Vanilla Greek Yogurt + Fresh Blueberries +
-                        Granola + Honey.
+                    <p className=''>
+                        {description}
                     </p>
                 </div>
                 {/*  <!-- Action base sized basic button --> */}
                 <div className="flex justify-end p-6 pt-0">
                     <button className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded bg-emerald-500 px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-emerald-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-                        <span>Order now!</span>
+                        <span>View Details</span>
                     </button>
                 </div>
             </div>
--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Service from './Service';
 
+
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() =>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+
     return (
-        <div className='grid md:grid-cols-3 gap-10 md:mx-36 mx-10'>
-            <Service></Service>
-            <Service></Service>
-            <Service></Service>
-            <Service></Service>
-            <Service></Service>
+        <div className='grid md:grid-cols-3 gap-10 md:mx-36 mx-10 my-20'>
+            {
+                services.map(service => <Service 
+                key={service.id}
+                service={service}
+                >
+                </Service>)
+            }
         </div>
     );
 };
